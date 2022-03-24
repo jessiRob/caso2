e7a801793441341a385152f52bdc3d5eecc86014
Jessica Robles <j.roblesm@uniandes.edu.co>
2022-03-23 19:46:28 -0500

    first commit

---
+++ src/caso2/prueba.java
@@ -0,0 +1,10 @@
+package caso2;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		long numeroDecimal = Long.parseLong("10001001", 2);
+		System.out.println(numeroDecimal);
+	}
+
+}
